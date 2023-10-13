21dcb460dc8b41f8e11b446595fa8c4a4806b210
Peter Fabi <kehindetemitayo.b@gmail.com>
2023-10-13 15:52:48 +0100

    Second commit
    Home page done

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -5,7 +5,33 @@ import { exerciseOptions, fetchData } from "../utils/fetchData";
 import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({setExercises, bodyPart, exercises}) => {
-  console.log(exercises)
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 9;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: 'smooth'})
+  }
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === 'all') {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+      }
+      setExercises(exercisesData)
+    }
+    fetchExercisesData();
+  }, [bodyPart])
+  
   return (
     <Box id='exercises'
       sx={{mt: {lg: '110px'}}}
@@ -16,7 +42,7 @@ const Exercises = ({setExercises, bodyPart, exercises}) => {
         Showing Results
       </Typography>
       <Stack direction={'row'} sx={{ gap: {lg: '110px', xs: '50px'}}} flexWrap={'wrap'} justifyContent={'center'}>
-        {exercises.map((exercise, index) => (
+        {currentExercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise}/>
         ))}
       </Stack>
@@ -26,7 +52,7 @@ const Exercises = ({setExercises, bodyPart, exercises}) => {
                 color="standard"
                 shape="rounded"
                 defaultPage={1}
-                count={Math.ceil(exercises.length/9)}
+                count={Math.ceil(exercises.length/exercisesPerPage)}
                 page={currentPage}
                 onChange={paginate}
                 size="large"
